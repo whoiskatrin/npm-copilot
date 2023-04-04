a479fc19851608bbaa35ce27df04b714f84c0464
“whoiskatrin” <katjareznikova@gmail.com>
2023-04-04 23:14:08 +0100

    debug

--- error-listener.js
+++ error-listener.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import { spawn } from "child_process";
-import logger from "./src/logger.js";
 import { handleErrors } from "./src/error-analyzer.js";
+import chalk from "chalk";
 
 const command = process.argv.slice(2);
 const childProcess = spawn("npm", ["run", "dev"], {
@@ -16,7 +16,7 @@ childProcess.stderr.on("data", async (data) => {
   const errorMsg = data.toString();
   const suggestion = await handleErrors(errorMsg);
   if (suggestion) {
-    logger.log("Suggested fix: " + suggestion);
+    console.log(chalk.green("Suggested fix: " + suggestion));
   }
 });
 
