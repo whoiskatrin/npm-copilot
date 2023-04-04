dd98f9669753e5fa60ea157a55b111992a28df4a
“whoiskatrin” <katjareznikova@gmail.com>
2023-04-04 20:02:36 +0100

    get errors from any source

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-#!/usr/bin/env node
-
 import { spawn } from "child_process";
 import logger from "./src/logger.js";
 import { handleErrors } from "./src/error-analyzer.js";
 
-const command = process.argv[2] || "dev";
-const nextProcess = spawn("npm", ["run", command], {
+const command = process.argv.slice(2);
+const childProcess = spawn(command[0], command.slice(1), {
   stdio: ["pipe", "pipe", "pipe"],
 });
 
-nextProcess.stdout.pipe(logger);
-nextProcess.stderr.pipe(logger);
+childProcess.stdout.pipe(process.stdout);
+childProcess.stderr.pipe(process.stderr);
 
-logger.on("data", async (data) => {
-  try {
-    const suggestion = await handleErrors(data);
-    if (suggestion) {
-      console.log(suggestion);
-    }
-  } catch (error) {
-    logger.error(error.message);
+const errorLogger = logger("error");
+
+childProcess.stderr.on("data", async (data) => {
+  const errorMessage = data.toString();
+  errorLogger(errorMessage);
+  const suggestion = await handleErrors(errorMessage);
+  if (suggestion) {
+    console.log(suggestion);
   }
 });
 
-nextProcess.on("exit", () => {
-  logger.end();
+childProcess.on("exit", () => {
+  errorLogger.end();
 });
 
 process.on("SIGINT", () => {
-  nextProcess.kill("SIGINT");
-  logger.end();
+  childProcess.kill("SIGINT");
+  errorLogger.end();
   process.exit();
 });
