4094fe6ffcc9d3269b9e1d75e690b1293e431ecf
“whoiskatrin” <katjareznikova@gmail.com>
2023-04-04 22:49:42 +0100

    debug

--- src/error-analyzer.js
+++ src/error-analyzer.js
@@ -6,42 +6,14 @@ const openaiApiKey = process.env.OPENAI_API_KEY;
 const OPENAI_ENDPOINT = "https://api.openai.com/v1/completions";
 
 async function handleErrors(logData) {
-  console.log("logData: " + logData);
+  const errorPattern = /Error:([\s\S]+?)\n\n/;
 
-  const errorKeywords = [
-    "Error:",
-    "UnhandledPromiseRejectionWarning:",
-    "Uncaught",
-    "Next.js",
-  ];
-
-  // Check if the log data contains any of the error keywords
-  const isError = errorKeywords.some((keyword) => logData.includes(keyword));
-
-  if (!isError) {
+  const errorMatch = logData.match(errorPattern);
+  if (!errorMatch) {
     return undefined;
   }
 
-  const errorLines = logData
-    .toString()
-    .split("\n")
-    .filter((line) => line.trim().startsWith("Error:"));
-
-  console.log("errorLines: " + errorLines);
-  if (errorLines.length === 0) {
-    return undefined;
-  }
-
-  const errorMessages = errorLines.map((line) => {
-    const message = line.trim().substring("Error:".length).trim();
-    const match = message.match(/^(.*)\s+\((.*):(\d+):(\d+)\)/);
-    if (match) {
-      const [_, errorMessage, fileName, lineNumber, columnNumber] = match;
-      return { errorMessage, fileName, lineNumber, columnNumber };
-    } else {
-      return { errorMessage: message };
-    }
-  });
+  const errorMessage = errorMatch[1].trim();
 
   const options = {
     method: "POST",
@@ -50,8 +22,8 @@ async function handleErrors(logData) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt: `Fix the following error:\n\n${errorMessages[0].errorMessage}\n\nSuggested fix:`,
-      model: "text-davinci-002",
+      prompt: `Fix the following error:\n\n${errorMessage}\n\nSuggested fix:`,
+      model: "text-davinci-003",
       temperature: 0.5,
       max_tokens: 147,
       top_p: 1,
