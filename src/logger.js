bdf7405bba166a9da404ac02b8afb3df74136590
“whoiskatrin” <katjareznikova@gmail.com>
2023-04-04 01:31:26 +0100

    fix: remove files

--- src/logger.js
+++ src/logger.js
@@ -3,12 +3,8 @@ import winston from "winston";
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "my-app" },
-  transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "combined.log" }),
-    new winston.transports.Console(),
-  ],
+  defaultMeta: { service: "npm-copilot" },
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
