6f8890f1d4008f9737d3acfa9ff06280a2f213f9
“whoiskatrin” <katjareznikova@gmail.com>
2023-04-06 20:10:12 +0100

    debug

--- error-listener.js
+++ error-listener.js
@@ -10,10 +10,7 @@ async function getProjectType() {
   const packageJsonPath = path.join(process.cwd(), "package.json");
 
   try {
-    const packageJsonData = await fs.promises.readFile(
-      packageJsonPath,
-      "utf-8"
-    );
+    const packageJsonData = await fs.readFile(packageJsonPath, "utf-8");
     const packageJson = JSON.parse(packageJsonData);
 
     if (packageJson.dependencies && packageJson.dependencies.next) {
@@ -60,7 +57,7 @@ function getPackageManager() {
   }
 }
 
-const projectType = getProjectType();
+const projectType = await getProjectType();
 const devCommandMap = {
   next: "dev",
   react: "start",
